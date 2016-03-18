278cd815de3efc661cf40b3c202ed5692e945602
Tobbe <tobsson@gmail.com>
2016-03-18 13:58:14 +0100

    Changed the code to listen for integers over he serial link. Added some code to listen for the values from RC and using them to control the car.

--- arduino_test_code/remoteControl/remoteControl.ino
+++ arduino_test_code/remoteControl/remoteControl.ino
@@ -6,16 +6,18 @@
 // --------- //
 // Constants //
 // --------- //
-//MOTOR
-const int servoPin = 9; //pin to which the servo motor is attached
-const int escPin = 6; //pin to which the ESC is attached
-const int rcPin = 3;
+const int servoPin = 9; // pin to which the servo motor is attached
+const int escPin = 6; // pin to which the ESC is attached
+const int rcPinSteer = 3; // rc steering
+const int rcPinESC = 5;  // rc motor
 
 // ----------------------- //
 // Instatiation of objects //
 // ----------------------- //
 Servo motor, steering;
 unsigned long rcControllerFlag;
+String input;
+int steer, velocity;
 
 void setup() {
   //SERIAL CONNECTION
@@ -29,19 +31,62 @@ void setup() {
 }
 
 void loop() {
-  rcControllerFlag = pulseIn(rcPin, HIGH, 25000);
-  Serial.print("Pulse read: ");
-  Serial.println(rcControllerFlag);
+  rcControllerFlag = pulseIn(rcPinSteer, HIGH, 25000); // if the timeout is lower it sometimes time out before getting a value
+  //Serial.print("Pulse read: ");
+  //Serial.println(rcControllerFlag);
   if(rcControllerFlag > 0){
-   motor.writeMicroseconds(1500);
-   steering.write(90);
+    //motor.writeMicroseconds(1500);
+    //steering.write(90);
+    rcControl();
+  }else{
+
+    manualControl();
+    //handleInput();
   }
-  handleInput();
+}
+
+void rcControl(){
+  steer = map(pulseIn(rcPinSteer, HIGH, 25000), 1000, 2000, 0, 180);
+  velocity = pulseIn(rcPinESC, HIGH, 25000);
+  int i;
+  int sum = 0;
+  for(i = 0; i < 10; i++){
+    sum += steer;
+  }
+  sum = sum / 10 + 7;
+  Serial.print("steer ");
+  Serial.println(floor(sum));
+  Serial.print("velocity ");
+  Serial.println(velocity);
+  steering.write(floor(sum));
+  motor.writeMicroseconds(3000 - velocity);
+}
+
+void manualControl(){
+  if (Serial.available()){
+    input = Serial.readStringUntil('\n');
+    
+    if (input.startsWith("s")){  // steering
+      steer = input.substring(1).toInt();
+      if (steer <= 180 && steer >=0){  // check that the value is in range
+        steering.write(steer);
+        Serial.println("Steering set to: ");
+        Serial.println(steer);
+      }
+    }else if (input.startsWith("v")){  // velocity
+      velocity = input.substring(1).toInt();
+      if (velocity <= 2000 && velocity >= 1000){  // check that the value is in range
+        motor.writeMicroseconds(velocity);
+        Serial.print("Velocity set to: ");
+        Serial.println(velocity);
+      }
+    }
+  }
 }
 
 void handleInput() { //handle serial input if there is any
   if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');
+    input = Serial.readStringUntil('\n');
     if (input.startsWith("w")) {
       motor.writeMicroseconds(1570);
       Serial.println("Going forward");
@@ -66,7 +111,7 @@ void handleInput() { //handle serial input if there is any
 
 void calibrateESC() {
   if (Serial.available()) {
-    String input = Serial.readStringUntil('\n');
+    input = Serial.readStringUntil('\n');
     if (input.startsWith("u")){
         motor.writeMicroseconds(2000);
         Serial.println("Full forward");
@@ -77,7 +122,6 @@ void calibrateESC() {
         motor.writeMicroseconds(1500);
         Serial.println("Neutral");
       }
-      
   }
 }
 
