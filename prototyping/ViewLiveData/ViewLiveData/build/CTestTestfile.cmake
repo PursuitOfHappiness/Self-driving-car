59c4ce4f9b6a5d7e47acdf48a4c728502090b706
Sergiu Ghita <sergiu.ghita@gmail.com>
2016-04-07 15:11:17 +0200

    Removed junk from viewlivedata prototype

--- prototyping/ViewLiveData/ViewLiveData/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chap/OpenDaVINCI/automotive/miniature/ViewLiveData
-# Build directory: /home/chap/OpenDaVINCI/automotive/miniature/ViewLiveData/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
