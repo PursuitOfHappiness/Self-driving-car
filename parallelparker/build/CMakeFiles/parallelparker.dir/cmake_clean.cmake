b0d4a5b5697f5910a41265fdffd19cc73157ea06
gusshail <gusshail@student.gu.se>
2016-04-26 12:52:50 +0200

    ParallelParker for simulator

---
+++ parallelparker/build/CMakeFiles/parallelparker.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/parallelparker.dir/apps/parallelparker.cpp.o"
+  "parallelparker.pdb"
+  "parallelparker"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/parallelparker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
