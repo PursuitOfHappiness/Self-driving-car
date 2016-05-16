306404dc71958297db05ff366b6412684d387b42
Tobbe <tobsson@gmail.com>
2016-05-16 15:17:09 +0200

    Deleted files that was not used for the presentation

--- prototyping/Testing-proxy-reports/arduino/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/chap/OpenDaVINCI/automotive/miniature/arduino
-# Build directory: /home/chap/OpenDaVINCI/automotive/miniature/arduino/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
